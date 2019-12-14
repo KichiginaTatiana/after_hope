c8b3b6c58703d1ab3aeb79ab90c37d59520b04c1
strstr <strifika@mail.ru>
2019-12-14 20:21:34 +0500

    поправила PersonParser

--- AfterHope.Data/Parsers/PersonParser.cs
+++ AfterHope.Data/Parsers/PersonParser.cs
@@ -1,20 +1,31 @@
 ﻿using System;
 using AfterHope.Data.Models;
+using AfterHope.Data.Repositories;
 
 namespace AfterHope.Data.Parsers
 {
     public class PersonParser : IPersonParser
     {
+        private readonly IPersonRepository personRepository;
+
+        public PersonParser(IPersonRepository personRepository)
+        {
+            this.personRepository = personRepository;
+        }
+
         public Person Parse(string data, string photoId)
         {
             var strings = data.Split("\n");
             if (strings.Length < 3)
                 throw new ArgumentException();
 
+            var id = ((uint) strings[0].GetHashCode()).ToString();
+            var person = personRepository.Read(id);
+
             return new Person
             {
                 PhotoId = photoId,
-                Id = ((uint) strings[0].GetHashCode()).ToString(),
+                Id = id,
                 Name = strings[0],
                 Lawsuit = strings[1],
                 Address = strings[2],
@@ -22,7 +33,8 @@ namespace AfterHope.Data.Parsers
                 Info = strings.Length > 4 ? strings[4] : null,
                 Type = strings.Length > 5 ? strings[5] : null,
                 Status = strings.Length > 6 ? strings[6] : null,
-                Created = DateTime.UtcNow
+                Created = person?.Created ?? DateTime.UtcNow,
+                Updated = DateTime.UtcNow
             };
         }
     }
